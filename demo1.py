0a9a57e29c551040ac57b3a2f5824d6c9565be22
richardyin123 <37510488+richardyin123@users.noreply.github.com>
2023-05-17 16:12:06 +0800

    feat(demo3):add demo3 function (#7) (#9)

--- demo1.py
+++ demo1.py
@@ -7,5 +7,10 @@ def demo2(name):
     print(f'say good afternoon to  {name}')
     return 'afternoon {name}'
 
+
+def demo3(name):
+    return f'good afternoon {name}'
+
+
 if __name__ == '__main__':
     demo1('richard')
